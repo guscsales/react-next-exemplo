2e8d3a7ca288a6817d78132711695a3f723c1f69
Gustavo Sales Santos <gssantos@uolinc.com>
2018-09-04 11:40:02 -0300

    first commit

---
+++ .next/4.85f761cbbac9723ded7e.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: /home/gssantos/workspace/react-next-exemplo/pages/index.js: Unterminated JSX contents (4:7)\n\n  2 | \t<div>\n  3 | \t\t<p>Hello Next.js\n> 4 | \t</div>\n    | \t      ^\n  5 | );\n  6 | \n  7 | export default Index;\n    at _class.raise (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:778:15)\n    at _class.jsxReadToken (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:7727:16)\n    at _class.readToken (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:8095:21)\n    at _class.nextToken (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:976:12)\n    at _class.next (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:912:10)\n    at _class.eat (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:917:12)\n    at _class.expect (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:2051:10)\n    at _class.jsxParseClosingElementAt (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:8001:12)\n    at _class.jsxParseElementAt (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:8020:39)\n    at _class.jsxParseElementAt (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:8024:34)\n    at _class.jsxParseElement (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:8077:19)\n    at _class.parseExprAtom (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:8084:21)\n    at _class.parseExprSubscripts (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:2725:21)\n    at _class.parseMaybeUnary (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:2704:21)\n    at _class.parseExprOps (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:2616:21)\n    at _class.parseMaybeConditional (/home/gssantos/workspace/react-next-exemplo/node_modules/babylon/lib/index.js:2588:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=4.85f761cbbac9723ded7e.hot-update.js.map
